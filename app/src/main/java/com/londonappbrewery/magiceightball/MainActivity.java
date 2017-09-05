ccc8d11734d5148c4d0deaf12046bf824f70375e
santhan456 <santhan2012@gmail.com>
2017-09-05 12:13:51 +0530

    Magic 8 ball by Santhan

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button spinButton = (Button) findViewById(R.id.spin);
+
+        final ImageView ballImage = (ImageView) findViewById(R.id.ball_image);
+
+        final int img[] = {R.drawable.ball1,R.drawable.ball2,
+                R.drawable.ball3,R.drawable.ball4,
+                R.drawable.ball5};
+
+        spinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int n1 = rand.nextInt(5);
+
+                ballImage.setImageResource(img[n1]);
+            }
+        });
+
     }
 }
